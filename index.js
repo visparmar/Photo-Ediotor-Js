1b39843f69cf70a196cb846fb24de4fa9ee4f53c
vikas <visparmar119@gmail.com>
2023-05-17 14:53:42 +0530

    feat: add functionality to filter container ans slidebar

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ let choose_img = document.querySelector(".btn");
 let choose_input = document.querySelector(".choose-image input");
 let imgSrc = document.querySelector(".your-photo-here img");
 let filter = document.querySelectorAll(".icon-container button");
-console.log(filter)
+let slider_info_name= document.querySelector(".name")
+let slider_info_value= document.querySelector(".value")
+let slider=document.querySelector(".slider input");
+
+let brightness =100 , contrast =100 ,saturate=100 ,opacity=100,blurx=0;
 
 choose_img.addEventListener('click', () => {
     choose_input.click()
@@ -32,24 +36,68 @@ filter.forEach((element) => {
     element.addEventListener('click', () => {
         document.querySelector('.active').classList.remove("active");
         element.classList.add("active")
+        slider_info_name.innerHTML=element.title
         if (element.id === "brightness") {
-            console.log("yes its a brightness")
+            slider.max="200"
+             slider.value=brightness;
+            slider_info_value.innerHTML=`${brightness}%`
+            
+            
         }
         else if (element.id === "contrast") {
-            console.log("yes its a contrast")
+            slider.max="200"
+            slider.value=contrast;
+            slider_info_value.innerHTML=`${contrast}%`
+            
         }
         else if (element.id === "saturate") {
-            console.log("yes its a saturate")
+            slider.max="200"
+            slider.value=saturate;
+            slider_info_value.innerHTML=`${saturate}%`
+        
         }
-        else if (element.id === "invert") {
-            console.log("yes its a invert")
+        else if (element.id === "opacity") {
+            slider.max="200"
+            slider.value=opacity;
+            slider_info_value.innerHTML=`${opacity}%`
+            
+            
         } else if (element.id === "blur") {
-            console.log("yes its a blur")
+            
+            slider.max="100"
+           slider.value=blurx;
+            slider_info_value.innerHTML=`${blurx}%`
+
+            
         }
 
     })
 
 })
 
+
+slider.addEventListener("input",()=>{
+    slider_info_value.innerHTML=`${slider.value}%`
+let slide_State=document.querySelector('.active');
+    if(slide_State.id==='brightness'){
+        brightness=slider.value
+        
+    }
+    else if(slide_State.id==='contrast'){
+        contrast=slider.value
+    }
+    else if(slide_State.id==='saturate'){
+         saturate=slider.value
+    }
+    else if(slide_State.id==='opacity'){
+        opacity=slider.value
+   }
+   else if(slide_State.id==='blur'){
+    blurx=slider.value
+}
+    imgSrc.style.filter=`contrast(${brightness}%)  brightness(${contrast}%) saturate(${saturate}%) opacity(${opacity}%) blur(${blurx}px)`
+    console.log(brightness,contrast,opacity,saturate,blurx)
+    
+})
 
 

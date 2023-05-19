a039d2af9d4f6da0dfd0b4fe7e32795b8e87fee8
vikas <visparmar119@gmail.com>
2023-05-19 15:49:08 +0530

    feat: Add rotate nd fip option

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ let filter = document.querySelectorAll(".icon-container button");
 let slider_info_name= document.querySelector(".name")
 let slider_info_value= document.querySelector(".value")
 let slider=document.querySelector(".slider input");
+let rotate_flip=document.querySelectorAll(".icon-container2 img");
 
 let brightness =100 , contrast =100 ,saturate=100 ,opacity=100,blurx=0;
+let degree=0;
+let toggle=0,toggle_up=0;
 
 choose_img.addEventListener('click', () => {
     choose_input.click()
@@ -74,6 +77,55 @@ filter.forEach((element) => {
     })
 
 })
+console.log(rotate_flip)
+rotate_flip.forEach((element)=>{
+    console.log(element);
+    element.addEventListener('click',()=>{
+        if(element.id==='rotate_right'){
+            if(degree===360){
+                degree=20;
+            }
+            // console.log(element.id)
+            degree=degree+20;
+            // console.log(degree)
+            
+        }
+        else if(element.id==='rotate_left'){
+
+            if(degree==-360){
+                degree=-20
+            }
+            // console.log(neg_deg)
+            degree=degree+(-20);
+            // console.log(degree)
+            
+
+        }
+        else if(element.id==='flip_right'){
+            if(toggle===0){
+                imgSrc.style.transform= 'scaleX(-1)'
+                toggle=1;
+            }else{
+                imgSrc.style.transform= ''
+                toggle=0;
+            }
+            
+        }
+
+        else if(element.id==='flip_up'){
+if(toggle===0){
+                imgSrc.style.transform= 'scaleY(-1)'
+                toggle=1;
+            }else{
+                imgSrc.style.transform= ''
+                toggle=0;
+            }
+        }
+
+        imgSrc.style.rotate=`${degree}deg`;
+    })
+
+})
 
 
 slider.addEventListener("input",()=>{
@@ -96,7 +148,7 @@ let slide_State=document.querySelector('.active');
     blurx=slider.value
 }
     imgSrc.style.filter=`contrast(${brightness}%)  brightness(${contrast}%) saturate(${saturate}%) opacity(${opacity}%) blur(${blurx}px)`
-    console.log(brightness,contrast,opacity,saturate,blurx)
+    // console.log(brightness,contrast,opacity,saturate,blurx)
     
 })
 
